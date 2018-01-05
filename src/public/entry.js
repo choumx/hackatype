063a739702ca7b5454ecb8974be307ba5a935777
William Chou <willchou@google.com>
2018-01-04 20:37:44 -0500

    add csp

--- src/public/entry.js
+++ src/public/entry.js
@@ -5,16 +5,20 @@ Promise.all([
   fetch('monkey.js').then((response) => response.text()),
   fetch('app.js').then((response) => response.text()),
 ]).then(([undom, monkey, app]) => {
-  let code = [
+  // CSP should disallow this.
+  const trickyGlobal = 'debugger; try { const g = Function("return this")() || (0, eval)("this"); console.assert(!g); } catch (e) {}';
+
+  const code = [
     undom,
     monkey,
     '(function() {', // Set `this` to `monkeyScope`.
     '  with (monkeyScope) {', // Shadow global vars e.g. `self`.
-    '    debugger;',
-         app,
+        trickyGlobal,
+        app,
     '  }',
     '}).call(monkeyScope);',
   ].join('\n');
+
   const blob = new Blob([code]);
   const worker = new Worker(URL.createObjectURL(blob), {type: 'module'}); // `module` doesn't work in Chrome yet (crbug/680046).
   renderer({worker});
